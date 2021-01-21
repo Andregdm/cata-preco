b6a189dd55d4aefa4ef9aec92a18e9dadc36faf6
Andregdm <andreg.dmenezes@gmail.com>
2021-01-21 11:07:43 -0300

    pagina tempo com opcao dinamica e estatica e revalidate

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,32 @@
 import { useState } from 'react';
+import Link from 'next/Link';
+
 
 function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Contador />
+            <Link href="/sobre">
+                <a>Sobre</a> 
+            </Link>
+            {/* <Contador /> */}
         </div>
     )
 }
 
-function Contador() {
-    const [contador,setContador] = useState(1);
+// function Contador() {
+//     const [contador,setContador] = useState(1);
 
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
+//     function adicionarContador() {
+//         setContador(contador + 1);
+//     }
 
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <div>{contador}</div>
+//             <button onClick={adicionarContador}>Adicionar</button>
+//         </div>
+//     )
+// }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import Link from 'next/Link';
+
+function Sobre() {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+            {/* <Contador /> */}
+        </div>
+    )
+}
+
+export default Sobre
