5c0c9e8e68befba68f2f063f11de24fb98ec99f0
Andregdm <andreg.dmenezes@gmail.com>
2021-01-21 12:46:19 -0300

    apiSecret 1 resultado da busca

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -3,8 +3,8 @@ async function tempo(request, response) {
     const dynamicDate = new Date();
     const buscaProd = await fetch(`https://catalogo-bff-v1-americanas.b2w.io/graphql?operationName=pageSearch&variables=${apiSecret}`);
     const buscaProdJson = await buscaProd.json();
-    const nome1item = buscaProdJson.data.search.products[20].product.name;
-    const preco1item = buscaProdJson.data.search.products[20].product.offers.result[0].salesPrice;
+    const nome1item = buscaProdJson.data.search.products[0].product.name;
+    const preco1item = buscaProdJson.data.search.products[0].product.offers.result[0].salesPrice;
     
     response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
