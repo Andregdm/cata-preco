40b95347a524d90997eb6b231405e3c690f58858
Andregdm <andreg.dmenezes@gmail.com>
2021-01-21 11:15:46 -0300

    comentarios removidos

--- pages/index.js
+++ pages/index.js
@@ -9,24 +9,24 @@ function Home() {
             <Link href="/sobre">
                 <a>Sobre</a> 
             </Link>
-            {/* <Contador /> */}
+            <Contador />
         </div>
     )
 }
 
-// function Contador() {
-//     const [contador,setContador] = useState(1);
+function Contador() {
+    const [contador,setContador] = useState(1);
 
-//     function adicionarContador() {
-//         setContador(contador + 1);
-//     }
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
 
-//     return (
-//         <div>
-//             <div>{contador}</div>
-//             <button onClick={adicionarContador}>Adicionar</button>
-//         </div>
-//     )
-// }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
 
 export default Home
